7def3f7bbf07286eed826c97cca03d62056b3e8a
cebicells-spec <cebicells@gmail.com>
2026-05-02 14:53:36 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+const mineflayer = require('mineflayer');
+
+const bot = mineflayer.createBot({
+  host: 'TrineMace.aternos.me',
+  port: 31986,
+  username: 'TrineField',
+  version: '26.1' // Match your server version exactly
+});
+
+bot.on('login', () => console.log('Bot connected'));
+bot.on('kicked', (reason) => console.log('Kicked:', reason));
+bot.on('error', (err) => console.log('Error:', err));
+
+// Auto-reconnect on disconnect
+bot.on('end', () => {
+  setTimeout(() => {
+    bot.createBot({ host: 'TrineMace.aternos.me', port: 31986, username: 'TrineField', version: '26.1' });
+  }, 5000);
+});   
